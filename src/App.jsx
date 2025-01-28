0f551fb5f5a3719c895ca1d949a5c20e30973991
vian <salahian.vian@gmail.com>
2025-01-28 00:24:18 -0800

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import images from './images.jsx';
-import Profile   from "./profile.jsx";
+import Profile from "./profile.jsx";
 import './App.css'
 
 function App() {
     return (
         <div style={{display:'flex',flexWrap:'wrap',justifyContent:'center'}}>
             <Profile name="Rowan Atkinson" description="Mr bean is a british sitcom." image={images.mrbean}/>
-            <Profile name="Marylin Monoroe" description="she was an American actress and model." image={images.maryam}/>
+            <Profile name="Marylin Monoroe" description="she was an American actress and model." image={images.marilyn}/>
             <Profile name="Cilian Murphy" description="Irish actor known for his intense performances." image={images.cilian}/>
-            <Profile name="Maryam Mirzakhani" description="she was an American actress and model." image={images.marilyn}/>
+            <Profile name="Maryam Mirzakhani" description="Maryam Mirzakhani was an Iranian mathematician and a professor of mathematics." image={images.maryam}/>
             <Profile name="Wiliam Bradley Pitt" description="He is an American actor and film producer." image={images.bradpitt}/>
             <Profile name="Tom Hardy" description=" He is an English actor, producer, and screenwriter." image={images.hardy}/>
             <Profile name="Kylian Mbappé" description="He is a French professional footballer." image={images.nolan}/>
