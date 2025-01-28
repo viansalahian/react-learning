7e9087cb85c1698808ffcc3dd6ff0534d962cc98
vian <salahian.vian@gmail.com>
2025-01-28 00:07:19 -0800

    profile task

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
-import { useState } from 'react';
-import './App.css';
+import images from './images.jsx';
+import Profile   from "./profile.jsx";
+import './App.css'
 
 function App() {
-    const [text, setText] = useState('');
-    const [display, setDisplay] = useState('');
-
-    const handleClick = () => {
-        setDisplay(text);
-        setText('');
-    };
-
     return (
-        <div className="container">
-            <h3>{display}</h3>
-            <div className="inner-container">
-                <input
-                    className="input-box"
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                    placeholder="Enter a text"
-                />
-                <div className="btn" onClick={handleClick}>Show Text</div>
-            </div>
+        <div style={{display:'flex',flexWrap:'wrap',justifyContent:'center'}}>
+            <Profile name="Rowan Atkinson" description="Mr bean is a british sitcom." image={images.mrbean}/>
+            <Profile name="Marylin Monoroe" description="she was an American actress and model." image={images.maryam}/>
+            <Profile name="Cilian Murphy" description="Irish actor known for his intense performances." image={images.cilian}/>
+            <Profile name="Maryam Mirzakhani" description="she was an American actress and model." image={images.marilyn}/>
+            <Profile name="Wiliam Bradley Pitt" description="He is an American actor and film producer." image={images.bradpitt}/>
+            <Profile name="Tom Hardy" description=" He is an English actor, producer, and screenwriter." image={images.hardy}/>
+            <Profile name="Kylian Mbappé" description="He is a French professional footballer." image={images.nolan}/>
+            <Profile name="Christopher Nolan" description="He is a British-American filmmaker and Hollywood director." image={images.mbape}/>
+            <Profile name="Ebrahim Hamedi" description="Ebi, is an Iranian pop singer ." image={images.ebi}/>
+
+
         </div>
     );
 }
